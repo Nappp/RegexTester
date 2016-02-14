656274cd976de28dcbce253e7082098e7dbfde3e
吴鹏煜 <napsterwu@gmail.com>
2016-02-14 14:46:09 +0800

    indent using spaces

--- generator.js
+++ generator.js
@@ -7,65 +7,65 @@ var topdomain = [".com", ".com.cn", ".com.hk", ".hk", ".net"];
 var chars = "abcdefghijklmnopqrstuvwxyz0123456789-";
 
 var rand = function(downer, upper){
-	if(arguments.length === 1){
-		upper = downer;
-		downer = 0;
-	}
-	return Math.floor( Math.random() * (upper - downer) + downer );
+  if(arguments.length === 1){
+    upper = downer;
+    downer = 0;
+  }
+  return Math.floor( Math.random() * (upper - downer) + downer );
 };
 
 var randStr = function(downer, upper){
-	if(arguments.length === 1){
-		upper = downer;
-		downer = 0;
-	}
-	var length = rand(downer, upper), strlen = chars.length;
-	var result = "";
-	while(length--){
-		result += chars[rand(strlen)];
-	}
-	return result;
+  if(arguments.length === 1){
+    upper = downer;
+    downer = 0;
+  }
+  var length = rand(downer, upper), strlen = chars.length;
+  var result = "";
+  while(length--){
+    result += chars[rand(strlen)];
+  }
+  return result;
 };
 
 var createHost = function(){
-	return randStr(3, 10) + topdomain[rand(topdomain.length)];
+  return randStr(3, 10) + topdomain[rand(topdomain.length)];
 };
 
 var createPath = function(){
-	var length = rand(4), result = "";
-	while(length--){
-		result += ("/" + randStr(4, 6));
-	}
-	return result;
+  var length = rand(4), result = "";
+  while(length--){
+    result += ("/" + randStr(4, 6));
+  }
+  return result;
 };
 
 var createQuery = function(){
-	var length = rand(4), result = length>0?"?":"";
-	while(length--){
-		result += ( randStr(4, 6) + "=" + randStr(4, 6) + (length>0?"&":"") );
-	}
-	return result;
+  var length = rand(4), result = length>0?"?":"";
+  while(length--){
+    result += ( randStr(4, 6) + "=" + randStr(4, 6) + (length>0?"&":"") );
+  }
+  return result;
 };
 
 var createHash = function(){
-	var result = randStr(5);
-	return ( (result.length>0?"#":"") + result );
+  var result = randStr(5);
+  return ( (result.length>0?"#":"") + result );
 };
 
 var createRecord = function(){
-	return protocols[rand(protocols.length)] + createHost() + createPath() + createQuery() + createHash();
+  return protocols[rand(protocols.length)] + createHost() + createPath() + createQuery() + createHash();
 
 };
 
 var file = fs.createWriteStream('records.txt');
 for(var i=0; i<COUNT; i++){
-	file.write(createRecord() + "\n", 'utf-8', (err) => {
-		if(err) throw err;
-	});
+  file.write(createRecord() + "\n", 'utf-8', (err) => {
+    if(err) throw err;
+  });
 }
 file.end();
 file.on('finish', (err) => {
-	if(err) console.error(err);
-	console.log('Write finished');
+  if(err) console.error(err);
+  console.log('Write finished');
 });
 
--- parser.js
+++ parser.js
@@ -2,9 +2,9 @@ var readline = require('readline');
 var fs = require('fs');
 
 const rl = readline.createInterface({
-	input: fs.createReadStream('records.txt'),
-	output: process.stdout,
-	terminal: false
+  input: fs.createReadStream('records.txt'),
+  output: process.stdout,
+  terminal: false
 });
 // const exp = /(?:(\w+):\/{0,2})?((?:(?:(?:\w|[%-])+)\.?)+\.(?:(?:\w|[%-])+))(?::(\d{0,5}))?((?:(?:\/(?:(?:\w|[%-])+))+)?(?:(?:\/)(?:(?:\w|[%-])+)(?:\.(?:(?:\w|[%-])+))?))?(?:\/?\??((?:\w|[=&%\[\]-])+))?(?:\/?#?((?:\w|[=&%-])+))?/
 
@@ -12,21 +12,21 @@ const rl = readline.createInterface({
 const c_exp = new RegExp(/(?:(\w+):\/{0,2})?((?:(?:(?:\w|[%-])+)\.?)+\.(?:(?:\w|[%-])+))(?::(\d{0,5}))?((?:(?:\/(?:(?:\w|[%-])+))+)?(?:(?:\/)(?:(?:\w|[%-])+)(?:\.(?:(?:\w|[%-])+))?))?(?:\/?\??((?:\w|[=&%\[\]-])+))?(?:\/?#?((?:\w|[=&%-])+))?/);
 
 var parse = (url) => {
-	var result = c_exp.exec(url);
-	// var result = url.match(exp);
+  var result = c_exp.exec(url);
+  // var result = url.match(exp);
 
-	return result && {
-		"url": result[0],
-		"protocol": result[1],
-		"host": result[2],
-		"port": result[3],
-		"path": result[4],
-		"query": result[5],
-		"hash": result[6]
-	} || null;
+  return result && {
+    "url": result[0],
+    "protocol": result[1],
+    "host": result[2],
+    "port": result[3],
+    "path": result[4],
+    "query": result[5],
+    "hash": result[6]
+  } || null;
 };
 
 rl.on('line', (line) => {
-	parse(line);
-	// console.log(parse(line));
+  parse(line);
+  // console.log(parse(line));
 });
